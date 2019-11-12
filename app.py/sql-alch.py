dd1d4d6be1622ae8cb4e62e17aea0e1651f1f076
racheltrindle <54429023+racheltrindle@users.noreply.github.com>
2019-11-12 07:08:33 -0800

    Add files via upload

--- app.py/sql-alch.py
+++ app.py/sql-alch.py
@@ -1,50 +1,78 @@
 
-# import numpy as np
+import numpy as np
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+import datetime as dt
 
 from flask import Flask, jsonify
 
-engine = create_engine("sqlite://hawaii.sqlite")
+engine = create_engine("sqlite:///Homework/Instructions/Resources/hawaii.sqlite")
 Base = automap_base()
 # reflect the tables
 Base.prepare(engine, reflect=True)
-
-stations = Base.classes.hawaii_stations.csv
-measurements=Base.classes.hawaii_measurements.csv
+Station = Base.classes.station
+Measurement=Base.classes.measurement
 
 app = Flask(__name__)
 
-# # @app.route("/")
-# # def welcome():
-# #     """List all available api routes."""
-# #     return (
-# #         f"Available Routes:<br/>"
-# #     f"api/v1.0/precipitation"
-# #     f"api/v1.0/stations"
-# #     f"api/v1.0/tobs"
-# #     f"api/v1.0/<start>"
-# #     f"api/v1.0/<start>/<end>")
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"api/v1.0/precipitation<br/>"
+        f"api/v1.0/station<br/>"
+        f"api/v1.0/tobs<br/>"
+        f"api/v1.0/start<br/>"
+        f"api/v1.0/start/end<br/>")
 
-# # @app.route("/api/v1.0/measurements")
-# # def precipitation():
-# #     # Create our session (link) from Python to the DB
-# #     session = Session(engine)
-# #     # Query all passengers
-# #     results = session.query(precipitation.date, precipitation.prcp).all()
-# #     session.close()
-# #     # Convert list of tuples into normal list
-# #     all_precipitation = []
-# #     for date, prcp in results:
-# #         prcp_dict = {}
-# #         prcp_dict["date"] = date
-# #         prcp_dict["prcp"] = prcp
-# #         all_precipitation.append(prcp_dict)
+@app.route("/api/v1.0/measurements")
+def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    # Query all passengers
+    results = session.query(Measurement.date, Measurement.prcp).all()
+    session.close()
+    # Convert list of tuples into normal list
+    all_precipitation = []
+    for date, prcp in results:
+        measurement_dict = {}
+        measurement_dict["date"] = date
+        measurement_dict["prcp"] = prcp
+        all_precipitation.append(measurement_dict)
+    return jsonify( all_precipitation)
 
+@app.route("/api/v1.0/station")
+def weather():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    # Query all passengers
+    results1 = session.query(Station.station, Station.name).all()
+    session.close()
+    # Convert list of tuples into normal list
+    all_weather= []
+    for station, name in results1:
+        station_dict = {}
+        station_dict["name"] = name
+        station_dict["station"] = station
+        all_weather.append(station_dict)
+    return jsonify(all_weather)
 
-# #     return jsonify( all_precipitation)
+@app.route("/api/v1.0/tobs")
+def temp():
+    session = Session(engine)
+    results2 = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date>= '2010-01-01').filter(Measurement.date<= '2010-12-31').order_by(Measurement.date).all()
+    session.close()
+    all_temp=[]
+    for date, tobs in results2:
+        measurement_dict = {}
+        measurement_dict["date"] = date
+        measurement_dict["tobs"] = tobs
+        all_temp.append(measurement_dict)
+    return jsonify(all_temp)
+
 
 
 if __name__ == "__main__":
